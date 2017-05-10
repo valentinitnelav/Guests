a29d264a251de3a26504fad734bf47b85c7a864a
valentinitnelav <stefan.valentin87@yahoo.com>
2017-05-10 16:51:40 +0200

    plot with ggplot

--- Raelene_Crandall/Graph_09May17/Scripts/Fig1D_num_seedlings.R
+++ Raelene_Crandall/Graph_09May17/Scripts/Fig1D_num_seedlings.R
@@ -1,6 +1,10 @@
 # install.packages("data.table")
 library(data.table)
+library(ggplot2)
 
+# ==================================
+# Read & prepare data
+# ==================================
 myDT <- fread("Data/Ailanthus year 2 post-fire.csv")
 str(myDT)
 myDT.noNA <- myDT[!is.na(Survival2)]
@@ -14,7 +18,11 @@ myDT.all <- myDT[,.(all=.N),by = .(Trt, Burn)]
 myDT.gr.all <- cbind(myDT.gr, myDT.all[,.(all)])
 myDT.gr.all <- myDT.gr.all[Trt != "All"]
 
+myDT.gr.all[, prob.surv := alive/total]
 
+# ==================================
+# Bootstrap CIs
+# ==================================
 bootstrap.CI.percentile <- function(x, N = 1000, CI = 0.95){
     stopifnot(is.vector(x), N > 0, is.numeric(CI), CI > 0, CI < 1)
     rep_avg <- replicate(N, mean(sample(x, size = length(x), replace = TRUE), na.rm = TRUE))
@@ -22,9 +30,9 @@ bootstrap.CI.percentile <- function(x, N = 1000, CI = 0.95){
     low.prc <- (1 - CI)/2
     up.prc  <- 1 - (1 - CI)/2
     low.CI  <- rep_avg[round(low.prc * N)]
-    names(low.CI) <- paste0("low.CI:", low.prc * 100, "%")
+    names(low.CI) <- "low.CI"
     up.CI   <- rep_avg[round(up.prc * N)]
-    names(up.CI) <- paste0("up.CI:", up.prc * 100, "%")
+    names(up.CI) <- "up.CI"
     return(c(low.CI, up.CI))
 }
 # Example
@@ -52,9 +60,67 @@ CIs <- lapply(unique(myDT.gr.all[,Trt]), function(trt){
 CIs <- rbindlist(CIs)
 
 myDT.gr.all <- merge(myDT.gr.all, CIs, by = c("Trt", "Burn"))
-myDT.gr.all
 
+# ==================================
+# Plot barplot with ggplot
+# ==================================
+ggplot(data = myDT.gr.all, 
+       aes(x    = Trt, 
+           y    = prob.surv, 
+           fill = Burn)) +
+    
+    # add the bars (means)
+    geom_bar(stat="identity", position=position_dodge()) +
+    # fill the bars manually
+    scale_fill_manual(name   = "Burned",
+                      breaks = c("N", "Y"),
+                      values = c("N" = "gray70", 
+                                 "Y" = "gray40"),
+                      labels = c("Unburned", "Burned")) + 
+    
+    # plot CIs (add a point shape as well)
+    geom_errorbar(aes(ymax=up.CI, ymin=low.CI), size=.4, width=.15, linetype="solid", position=position_dodge(.9)) +
+    geom_point(position=position_dodge(width=.9), shape="-", show.legend = FALSE) +
+    
+    # set order of discrete values on OX axes & adjust the distance (gap) from OY axes
+    scale_x_discrete(limits = c("Control", "Comp", "Herb"),
+                     labels = c("Control", "Competitor \n Removal", "Herbivore \n Removal"),
+                     expand = c(0, .5)) +
+    # set range on OY axes and adjust the distance (gap) from OX axes
+    scale_y_continuous(limits = c(0, 1), expand = c(0, 0)) +
+    
+    # Final adjustments:
+    # set axis labels
+    labs(x = "", 
+         y = "Probability of Survival") +
+    theme_bw() + # eliminate default background 
+    theme(panel.grid.major = element_blank(), # eliminate major grids
+          panel.grid.minor = element_blank(), # eliminate minor grids
+          # set font family for all text within the plot ("serif" should work as "Times New Roman")
+          # note that this can be overridden with other adjustment functions below
+          text = element_text(family="serif"),
+          # adjust X-axis title
+          axis.title.x = element_text(size = 10, face = "bold"),
+          # adjust X-axis labels
+          axis.text.x = element_text(size = 10, face = "bold", color="black"),
+          # adjust Y-axis title
+          axis.title.y = element_text(size = 10, face = "bold"),
+          # adjust legend title appearance
+          legend.title = element_text(size = 8, face = "bold"),
+          # adjust legend label appearance
+          legend.text = element_text(size = 8, face = "plain"),
+          # change spacing between legend items
+          legend.key.height = unit(4, "mm"),
+          # don't draw legend box (check element_rect() for borders and backgrounds)
+          legend.background = element_blank(),
+          # Put upper-left corner of legend box in upper-left corner of graph
+          # Note that the numeric position in legend.position below is relative to the entire area, 
+          # including titles and labels, not just the plotting area
+          legend.justification = c(0,1),
+          legend.position = c(0,1))
 
+# save as pdf
+ggsave("barplot with CI bars - ggplot.pdf", width=12, height=8, units="cm")
 
 # ===========================================
 # some part of the original code from Rae
